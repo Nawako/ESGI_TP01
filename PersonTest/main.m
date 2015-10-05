15c1224d15b937f104d90a91a6a19879a7928ab9
Etudiant <Etudiant@imac-de-etudiant-23.local>
2015-10-05 11:43:24 +0200

    Initial Commit

---
+++ PersonTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PersonTest
+//
+//  Created by Etudiant on 05/10/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
